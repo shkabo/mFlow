f658989368245a0346df92986d5e86cdebc1ecbf
Bosko Stupar <bosko.stupar@gmail.com>
2017-11-14 22:36:16 +0100

    in/out model

---
+++ backend/models/inout.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const user = require('./user');
+const product = require('./product');
+const project = require('./project');
+
+let inout = new Schema({
+  products: [
+    {
+      product_id: { type: Schema.Types.ObjectId, ref: "product" },
+      quantity: { type: Number }
+    }
+  ],
+  user_add: { type: Schema.Types.ObjectId, ref: "user" },
+  user_take: { type: Schema.types.ObjectId, ref: "user" },
+  project_id: { type: Schema.Types.ObjectId, ref: "project" },
+  created: { type: Date, default: Date.now }
+}, {
+  versionKey: false
+});
+
+inout.pre('save', (next) => {
+  now = new Date();
+  if (!this.created) {
+    this.created = now;
+  }
+  next();
+});
+
+module.exports = mongoose.model('inout', inout);
